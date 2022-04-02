16b3e7a8231faa235cab6d460e3164341f76ade6
JonasFiechter <jonasrfiechter@gmail.com>
2022-04-02 19:54:00 -0300

    update
    One more lecture

--- App.js
+++ App.js
@@ -6,7 +6,9 @@ import {
   View,
   Button,
   Linking,
-  isDarkMode
+  isDarkMode,
+  ScrollView,
+  RefreshControl,
 } from 'react-native';
 
 import {
@@ -15,26 +17,35 @@ import {
 
 const App = () => {
 
+  const [Items, setItems] = useState([
+    {key: 1, item: 'Item 1'},
+    {key: 2, item: 'Item 2'},
+    {key: 3, item: 'Item 3'},
+    {key: 4, item: 'Item 4'},
+    {key: 5, item: 'Item 5'},
+    {key: 6, item: 'Item 6'},
+    {key: 7, item: 'Item 7'},
+    {key: 8, item: 'Item 8'},
+    {key: 9, item: 'Item 9'},
+    {key: 10, item: 'Item 10'},
+  ])
+
   const backgroundStyle = {
     backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
   };
 
   return (
-    <View style={styles.body}>
-        <View style={styles.viewM1}>
-          <View style={styles.view1}><Text style={styles.text}>1</Text></View>
-          <View style={styles.view2}><Text style={styles.text}>2</Text></View>
-          <View style={styles.view3}><Text style={styles.text}>3</Text></View>
-        </View>
-        <View style={styles.viewM2}>
-          <View style={styles.view4}><Text style={styles.text}>4</Text></View>
-          <View style={styles.view5}><Text style={styles.text}>5</Text></View>
-        </View>
-        <View style={styles.viewM3}>
-          <View style={styles.view6}><Text style={styles.text}>6</Text></View>
-          <View style={styles.view7}><Text style={styles.text}>7</Text></View>
-        </View>
-    </View>
+      <ScrollView  style={styles.body} refreshControl={<RefreshControl/>}>
+        {
+          Items.map((item) => {
+            return (        
+            <View style={styles.view1} key={Object.key}>
+              <View style={styles.viewI}><Text style={styles.text}>{item.item}</Text></View>
+            </View>
+            )
+          })
+        }
+      </ScrollView>
   );
 };
 
@@ -43,47 +54,21 @@ const styles = StyleSheet.create({
     flex: 1,
     flexDirection: 'column',
     backgroundColor: '#000',
-    alignItems: 'stretch',
-    justifyContent: 'center',
   },
   text: {
     color: '#fff',
-    fontSize: 20,
-    padding: 10,
+    fontSize: 50,
+    margin: 10,
   },
   button: {
     margin: 5,
   },
-  viewM1: {
-    height: 50, flexDirection: 'row', alignItems: 'stretch'
-  },
-  viewM2: {
-    height: 100, flexDirection: 'column', alignItems: 'stretch'
-  },
-  viewM3: {
-    flex: 1, height: 100, flexDirection: 'row', alignItems: 'stretch'
-  },
-  view1: {
-    backgroundColor: '#355', flex: 1, alignItems: 'center', justifyContent: 'center',
-  },
-  view2: {
-    backgroundColor: '#345', flex: 2, alignItems: 'center', justifyContent: 'center',
-  },
-  view3: {
-    backgroundColor: '#335', flex: 3, alignItems: 'center', justifyContent: 'center',
-  },
-  view4: {
-    backgroundColor: '#225', flex: 1, alignItems: 'center', justifyContent: 'center',
-  },
-  view5: {
-    backgroundColor: '#215', flex: 1, alignItems: 'center', justifyContent: 'center',
-  },
-  view6: {
-    backgroundColor: '#204', flex: 1, alignItems: 'center', justifyContent: 'center',
-  },
-  view7: {
-    backgroundColor: '#103', flex: 1, alignItems: 'center', justifyContent: 'center',
-  },
+    viewI: {
+    margin: 10,
+    backgroundColor: '#253', 
+    alignItems: 'center',
+    justifyContent: 'center',
+  }
 });
 
 export default App;
